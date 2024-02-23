53b9e684fbff679e0bfab9f4bb21fbe55e68ab14
WJPMorbi <141144813+WJPMorbi@users.noreply.github.com>
2024-02-23 14:02:54 +0530

    1st version

---
+++ Javascript/GSJava.js
@@ -0,0 +1,46 @@
+// ------------------------- Cursor Events -------------------------
+// const cursorDot = document.querySelector(".cursor-dot");
+// const cursorOutine = document.querySelector(".cursor-outline");
+
+// window.addEventListener("mousemove",(e)=>{
+//   const posX = e.clientX;
+//   const posY = e.clientY;
+
+//   cursorDot.style.left = `${posX}px`;
+//   cursorDot.style.top = `${posY}px`;
+
+//   // cursorOutine.style.left = `${posX}px`;
+//   // cursorOutine.style.top = `${posY}px`;
+
+//   cursorOutine.animate({
+//     left : `${posX}px`,
+//     top : `${posY}px`
+//   },{
+//     duration:500, fill:"forwards"
+//   });
+// });
+
+// ------------------------- Toggle Function -------------------------
+function AnimeListItem(){
+    const NavItem  = document.querySelectorAll('.LandNav li');
+
+    NavItem.forEach((NavItem, index) => {
+        setTimeout(()=>{
+            NavItem.classList.remove('hidden');
+            NavItem.classList.add('visible');
+        }, (index+1)*300);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+   AnimeListItem(); 
+});
+
+// ------------------------- Change bg position -------------------------
+
+window.onload = function() {
+  let osho_2 = document.querySelector('.sub-header-osho');
+  
+  osho_2.style.backgroundPosition = "50% 50%";
+  osho_2.style.opacity = "1";
+};
---
+++ Javascript/main.js
@@ -0,0 +1,56 @@
+
+
+let masterPlay = document.getElementById('masterPlay');
+masterPlay.addEventListener('click',()=>{
+    if(music.paused || music.currentTime <= 0){
+        music.play();
+        masterPlay.classList.remove('ri-play-circle-fill');
+        masterPlay.classList.add('ri-pause-circle-fill');
+    }else {
+        music.pause();
+        masterPlay.classList.remove('ri-pause-circle-fill');
+        masterPlay.classList.add('ri-play-circle-fill');
+    }
+})
+
+let currentStart = document.getElementById('currentStart');
+let currentEnd = document.getElementById('currentEnd');
+let seek = document.getElementById('seek');
+let bar2 = document.getElementById('bar2');
+
+music.addEventListener('timeupdate',()=>{
+    let music_curr = music.currentTime;
+    let music_dur = music.duration;
+
+    let min1 = Math.floor(music_dur/60);
+    let sec1 = Math.floor(music_dur%60);
+
+    if (sec1<10) {
+        sec1 = `0${sec1}`;
+    }
+    currentEnd.innerText = `${min1}:${sec1}`;
+
+    let min2 = Math.floor(music_curr/60);
+    let sec2 = Math.floor(music_curr%60);
+
+    if (sec2<10) {
+        sec2 = `0${sec2}`;
+    }
+    currentStart.innerText = `${min2}:${sec2}`;
+
+    seek.value = parseInt((music_curr/music_dur)*100);
+    let seekbar = seek.value;
+    bar2.style.width = `${seekbar}%`;
+});
+
+seek.addEventListener('change',()=>{
+    music.currentTime = seek.value * music.duration / 100;
+})
+
+music.addEventListener('ended',()=>{
+    masterPlay.classList.remove('ri-pause-circle-fill');
+    masterPlay.classList.add('ri-play-circle-fill');
+    bar2.style.width = `${0}%`;
+    seek.value = 0;
+    currentStart.innerText = '0:00';
+})
