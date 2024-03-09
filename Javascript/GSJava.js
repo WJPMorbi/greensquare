717e48f5d989691cbffce28cf4b10bc27497652f
WJPMorbi <141144813+WJPMorbi@users.noreply.github.com>
2024-03-09 13:23:39 +0530

    w360 update

--- Javascript/GSJava.js
+++ Javascript/GSJava.js
@@ -36,4 +36,27 @@ document.addEventListener("DOMContentLoaded", function(event){
    AnimeListItem(); 
 });
 
+// ------------------------- Adding icon links -------------------------
+
+let icon_links = document.querySelectorAll('.ft-cont-main')[2];
+let navIcon_Links = document.querySelectorAll('.nav-con-icon')[0];
+
+let insta_link = icon_links.childNodes[1];
+let fb_link = icon_links.childNodes[3];
+let yt_link = icon_links.childNodes[5];
+
+insta_link.setAttribute("href","https://www.instagram.com/santhaliya_pradeep?igsh=ZDE2bDNmcnM2ZXgw");
+fb_link.setAttribute("href","https://www.facebook.com/santhaliyapradeep?mibextid=ZbWKwL");
+yt_link.setAttribute("href","https://youtube.com/@PradeepWorld");
+
+let nav_insta_link = navIcon_Links.childNodes[1];
+let nav_fb_link = navIcon_Links.childNodes[3];
+let nav_yt_link = navIcon_Links.childNodes[5];
+
+nav_insta_link.setAttribute("href","https://www.instagram.com/santhaliya_pradeep?igsh=ZDE2bDNmcnM2ZXgw");
+nav_fb_link.setAttribute("href","https://www.facebook.com/santhaliyapradeep?mibextid=ZbWKwL");
+nav_yt_link.setAttribute("href","https://youtube.com/@PradeepWorld");
+
+
+
 
