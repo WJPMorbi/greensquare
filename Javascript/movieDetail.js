4c8409b6b786388742e03b2cd201d53bfda983d6
WJPMorbi <141144813+WJPMorbi@users.noreply.github.com>
2024-04-14 11:59:43 +0530

    movie list title

---
+++ Javascript/movieDetail.js
@@ -0,0 +1,18 @@
+const movie_detail = [
+    {
+        srN:"1",
+        movieName:"Coco",
+        time:"140 min",
+        Director:"John Cartor",
+        genre:"Action, drama",
+        fill: 4,
+        line: 1,
+        story:"lorem ipsume",
+        img1:"",
+        video1:"",
+        country:"india",
+        language:"hindi",
+        releaseDate:"13 jul 2022"
+    }
+]
+
