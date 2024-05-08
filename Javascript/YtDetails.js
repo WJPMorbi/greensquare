a308f1eaefb1dae389216323f746c3d5c2337431
WJPMorbi <141144813+WJPMorbi@users.noreply.github.com>
2024-05-08 18:42:21 +0530

    Youtube Pge

---
+++ Javascript/YtDetails.js
@@ -0,0 +1,118 @@
+let RajShamaniImg = document.getElementsByClassName('RajShamaniImg');
+
+Array.from(RajShamaniImg).forEach(Image => {
+    Image.src = "https://yt3.ggpht.com/sNTXQbT-tVdQ-D92xTN4iIfRoHPeh2geQRN8QSL-It07X8Zuy4WPhuUTQm9nZ11-MoOh_qbvyg=s176-c-k-c0x00ffffff-no-rj-mo";
+});
+
+
+const YtDetails = [
+    {
+        id:"1",
+        YtChannelLogo:"https://yt3.ggpht.com/sNTXQbT-tVdQ-D92xTN4iIfRoHPeh2geQRN8QSL-It07X8Zuy4WPhuUTQm9nZ11-MoOh_qbvyg=s176-c-k-c0x00ffffff-no-rj-mo",
+        YtChannelName:"Raj Shamani",
+        YtChannelPoster:"https://i.ytimg.com/vi/v9AHBtbk-E0/hqdefault.jpg",
+        YtCHannelTitle:"How To Attract Money, Love & Career? Manifest, Law Of Attraction - Mitesh & Indu | FO196 Raj Shamani",
+        YtCHannelViews:"450k+",
+        YtUrl:"https://www.youtube.com/watch?v=v9AHBtbk-E0&t=1390s&ab_channel=RajShamani",
+    },
+    {
+        id:"2",
+        YtChannelLogo:"https://yt3.ggpht.com/sNTXQbT-tVdQ-D92xTN4iIfRoHPeh2geQRN8QSL-It07X8Zuy4WPhuUTQm9nZ11-MoOh_qbvyg=s176-c-k-c0x00ffffff-no-rj-mo",
+        YtChannelName:"Raj Shamani",
+        YtChannelPoster:"https://i.ytimg.com/vi/zZ-VeqYPxoA/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBRc2yqh0LJZcCZEvCial_Fc41fPA",
+        YtCHannelTitle:"Dr. Velumani On Building ₹5000 Crore Business, Poverty, Risk & Success | FO174 | Raj Shamani",
+        YtCHannelViews:"1.6M+",
+        YtUrl:"https://www.youtube.com/watch?v=zZ-VeqYPxoA&ab_channel=RajShamani"
+    },
+    {
+        id:"3",
+        YtChannelLogo:"https://yt3.ggpht.com/ytc/AIdro_kn2s75bPw7S1AEPbb7dMX8Q-5Dm7JMHbPibS0o0oND9r0=s48-c-k-c0x00ffffff-no-rj",
+        YtChannelName:"Be inspired",
+        YtChannelPoster:"https://i.ytimg.com/vi_webp/PqMZfr3wMHE/hqdefault.webp",
+        YtCHannelTitle:"[CLASSIFIED] 'Only a Few People On Earth Know About It'",
+        YtCHannelViews:"10M+",
+        YtUrl:"https://m.youtube.com/watch?v=PqMZfr3wMHE&pp=ygUgYmUgaW5zcGlyZWQgb25seSBmZXcgcHdvcGx3IGtub3c%3D"
+    },
+    {
+        id:"4",
+        YtChannelLogo:"https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s68-c-k-c0x00ffffff-no-rj",
+        YtChannelName:"Dhruv Rathee",
+        YtChannelPoster:"https://i.ytimg.com/vi/KIvfM4g4aG4/hqdefault.jpg",
+        YtCHannelTitle:"Mystery of Flight 571 | World's Greatest Miracle | Dhruv Rathee",
+        YtCHannelViews:"17M+",
+        YtUrl:"https://m.youtube.com/watch?v=KIvfM4g4aG4&pp=ygUQbWlyYWNsZSBvZiBhbmRlcw%3D%3D"
+    },
+    {
+        id:"5",
+        YtChannelLogo:"https://yt3.ggpht.com/ytc/AIdro_kKGaEKaafkVObI-xJQPOipJ-B-W2vdD8fuv4oNEW7d894=s68-c-k-c0x00ffffff-no-rj",
+        YtChannelName:"National Geographic India",
+        YtChannelPoster:"https://i.ytimg.com/vi/2Hcc7BppqAE/hqdefault.jpg",
+        YtCHannelTitle:"A Home Away From Home | India's Mega Kitchens | National Geographic",
+        YtCHannelViews:"350k+",
+        YtUrl:"https://youtu.be/2Hcc7BppqAE?si=deRAJGqBCKhjkEOP"
+    },
+    {
+        id:"6",
+        YtChannelLogo:"https://yt3.ggpht.com/ytc/AIdro_n6L7_c7YGCVaOaWPQYtuDE-LKA6I4tZ_DgvS61nR28_HA=s68-c-k-c0x00ffffff-no-rj",
+        YtChannelName:"Ranveer Allahbadia",
+        YtChannelPoster:"https://i.ytimg.com/vi_webp/fCGqito67KA/hqdefault.webp",
+        YtCHannelTitle:"My HONEST Adventure Stories - Pyramids, Alien & Crime | Major Sushant Singh, TheRanveerShow...",
+        YtCHannelViews:"130k+",
+        YtUrl:"https://youtu.be/zQoYue_R9qg?si=qgF8ZjbNQzKMmU6e"
+    },
+    {
+        id:"7",
+        YtChannelLogo:"https://yt3.ggpht.com/ytc/AIdro_n6L7_c7YGCVaOaWPQYtuDE-LKA6I4tZ_DgvS61nR28_HA=s68-c-k-c0x00ffffff-no-rj",
+        YtChannelName:"Ranveer Allahbadia",
+        YtChannelPoster:"https://i.ytimg.com/vi_webp/Eqnrhwca77w/hqdefault.webp",
+        YtCHannelTitle:"Special Forces Aur Yeti Ki Kahaaniyan Ft. Major Sushant | TheRanveerShow...",
+        YtCHannelViews:"1M+",
+        YtUrl:"https://youtu.be/Eqnrhwca77w?si=ATNVmBJnEVqOizKW"
+    }
+]
+
+YtDetails.forEach(Video => {
+    const youtube = document.querySelector('.Youtube_video_container');
+
+    const YtCard = document.createElement('div');
+    YtCard.classList.add('Yt_card');    
+    YtCard.setAttribute('data-url', Video.YtUrl);
+
+    const YtChannelLogo = document.createElement('div');
+    YtChannelLogo.classList.add('yt_channel_logo')
+    const img = document.createElement('img');
+    img.src = Video.YtChannelLogo;
+    const H3Tag = document.createElement('h4');
+    H3Tag.innerText = Video.YtChannelName;
+    YtChannelLogo.appendChild(img);
+    YtChannelLogo.appendChild(H3Tag);
+
+    const img2 = document.createElement('img');
+    img2.src = Video.YtChannelPoster;
+
+    const H3Tag2 = document.createElement('h3');
+    H3Tag2.innerText = Video.YtCHannelTitle;
+    H3Tag2.classList.add('YtChTitle');
+
+    const viewsTag = document.createElement('p');
+    viewsTag.innerHTML = `<i class='ri-eye-fill'></i> ${Video.YtCHannelViews} Views`;
+
+    YtCard.appendChild(YtChannelLogo);
+    YtCard.appendChild(img2);
+    YtCard.appendChild(H3Tag2);
+    YtCard.appendChild(viewsTag);
+    youtube.appendChild(YtCard);
+    })
+
+    let Yt_card = document.querySelectorAll('.Yt_card');
+
+    Yt_card.forEach(card2 => {
+        card2.addEventListener('click',()=>{
+            const Yt_url = card2.getAttribute('data-url');
+            window.open(Yt_url, '_blank');
+        })
+    })
+
+
+
+    
